af3c61ac551aaa4986755dbd2ddd9ed2dde0c416
Kingz245 <80926790+Kingz245@users.noreply.github.com>
2021-03-18 22:33:33 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . a a a a a a . . . . . 
+. . . . a a 1 1 1 1 a a . . . . 
+. . . . a 1 1 5 5 1 1 a . . . . 
+. . . . a 1 1 5 5 1 1 a . . . . 
+. . . . a a 1 1 1 1 a a . . . . 
+. . . . . a a a a a a . . . . . 
+. . . . . . . 7 7 . . . . . . . 
+. . . . . . . 7 7 . . . . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . . . 7 7 . . . . . . . 
+. . . . . . . 7 7 . . . . . . . 
+`;
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . e . . . . . 
+. . . . . . . e e e e . . . . . 
+. . . . . . . . . . e . . . . . 
+. . . . . f 5 f 5 f 5 f 5 . . . 
+. . . . . . 5 f 5 f 5 f 5 . . . 
+. . . . . . . f 5 f 5 f 5 . . . 
+. . . . . . . e . . e . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
